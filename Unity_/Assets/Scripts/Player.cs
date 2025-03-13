3a6d5172705bde6ba3d6f39982ebf1f929943568
DurumSoloCarne <jesux98fc@gmail.com>
2025-03-13 18:31:02 +0100

    Camera + Player movement
    El movimiento del jugador funciona perfectamente, incluyendo las diagonales.
    La cámara sigue al jugador y si hay una pared (OutOfBounds) se para para no mostrarla.
    Aún falta el manejador de Cámara para cambios de plano en los que se teletransporte al jugador y haya una pared cerca.

--- Unity_/Assets/Scripts/Player.cs
+++ Unity_/Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
-    const float speed = 4f;
+    const float speed = 5f;
 
     public static Transform tf;
     public Rigidbody2D Rb;
+
+    void Awake(){
+        tf = transform;
+        Rb = GetComponent<Rigidbody2D>();
+    }
 
     void Start(){
         
@@ -13,32 +19,78 @@ public class Player : MonoBehaviour
 
     
     void Update(){
-        float MovementX = Input.GetAxis("Horizontal");
-        float MovementY = Input.GetAxis("Vertical");
-        Rb.linearVelocity = new Vector2(speed * MovementX, Rb.linearVelocity.y);
+        float MovementX = Input.GetAxisRaw("Horizontal") * speed;
+        float MovementY = Input.GetAxisRaw("Vertical") * speed;
 
-        if(Input.GetKey(KeyCode.A)){
-            Rb.linearVelocity = new Vector2(-speed, Rb.linearVelocity.y);
+        MovementHandler(MovementX,MovementY);
+        
+    }
+
+
+
+    //Manejador de movimiento del jugador
+    void MovementHandler(float MovementX, float MovementY){
+
+        Rb.linearVelocity = new Vector2(MovementX,MovementY);
+        
+        //Parar cuando se pulsen las teclas de direcciones opuestas
+        if(PulsandoTeclasContrarias('X')){
+            Rb.linearVelocity = new Vector2(0f,Rb.linearVelocity.y);
+        }
+        if(PulsandoTeclasContrarias('Y')){
+            Rb.linearVelocity = new Vector2(Rb.linearVelocity.x,0f);
         }
 
-        if(Input.GetKey(KeyCode.D)){
-            Rb.linearVelocity = new Vector2(speed, Rb.linearVelocity.y);
+        //Movimiento en diagonal
+        if(Diagonal()){
+            Rb.linearVelocity = new Vector2(MovementX,MovementY) / Mathf.Sqrt(2);
         }
+        
+    }
 
-        if(Input.GetKey(KeyCode.W)){
-            Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, speed);
-        }
 
-        if(Input.GetKey(KeyCode.S)){
-            Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, -speed);
+    //Se le pasa un char, 'X' o 'Y' dependiendo del Axis a comprobar
+    //Devuelve true si se estan pulsando las teclas de direcciones contrarias del Axis especificado
+    bool PulsandoTeclasContrarias(char Axis){
+        if(Axis == 'X'){
+            if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)){
+                return true;
+            }
         }
+        if(Axis == 'Y'){
+            if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S)){
+                return true;
+            }
+        }
+        return false;
+    }
 
-        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)){
-            Rb.linearVelocity = new Vector2(0f, Rb.linearVelocity.y);
-        }
 
-        if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)){
-            Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, 0f);
+    //Devuelve true si se está pulsando alguna tecla de dirección
+    bool PulsandoTecla(){
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+            return true;
         }
+        return false;
+    }
+
+
+    //Devuelve true si está caminando en una diagonal
+    bool Diagonal(){
+        if(!PulsandoTeclasContrarias('X') && !PulsandoTeclasContrarias('Y')){
+            if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
+                return true;
+            }
+            if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
+                return true;
+            }
+            if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
+                return true;
+            }
+            if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Unity_/Assets/Scripts/PlayerCamera.cs
+++ Unity_/Assets/Scripts/PlayerCamera.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    /*
-    const float RaycastDistanceX = 12.5f;
+    const float RaycastDistanceX = 12.7f;
     const float RaycastDistanceY = 7f;
 
     public Transform tf;
@@ -23,32 +22,31 @@ public class PlayerCamera : MonoBehaviour
             jugador_tf = jugador.transform;
         }
 
-        //Usamos un Raycast en cada direccion para recolocar la camara en caso de que haya una pared
-        Vector2 OriginRaycast = jugador_tf;
+        //La camara comienza en la posicion del jugador
+        tf.position = new Vector3(jugador_tf.position.x, jugador_tf.position.y, tf.position.z);
 
+        //Usamos un Raycast en cada direccion para recolocar la camara en caso de que haya una pared
+    }
+
+    void Update(){
+        Vector2 OriginRaycast = jugador_tf.position;
         RaycastHit2D R_left = Physics2D.Raycast(OriginRaycast,Vector2.left,RaycastDistanceX);
         RaycastHit2D R_up = Physics2D.Raycast(OriginRaycast,Vector2.up,RaycastDistanceY);
         RaycastHit2D R_right = Physics2D.Raycast(OriginRaycast,Vector2.right,RaycastDistanceX);
         RaycastHit2D R_down = Physics2D.Raycast(OriginRaycast,Vector2.down,RaycastDistanceY);
 
-        Debug.DrawRay(OriginRaycast, Vector2.left * RaycastDistanceX, Color.red); //Pintar Raycast
-        Debug.DrawRay(OriginRaycast, Vector2.up * RaycastDistanceY, Color.green); //Pintar Raycast
-        Debug.DrawRay(OriginRaycast, Vector2.right * RaycastDistanceX, Color.yellow); //Pintar Raycast
-        Debug.DrawRay(OriginRaycast, Vector2.down * RaycastDistanceX, Color.purple); //Pintar Raycast
-
-
-        //La camara comienza en la posicion del jugador
-        tf.position = jugador_tf;
-    }
-
-    void Update(){
-
         //Actualizamos la posicion de la camara con la del jugador
-        tf.position = jugador_tf;
+        if(R_left.collider == null && R_right.collider == null){
+            tf.position = new Vector3(jugador_tf.position.x, tf.position.y, tf.position.z);
+        }
+        if(R_up.collider == null && R_down.collider == null){
+            tf.position = new Vector3(tf.position.x, jugador_tf.position.y, tf.position.z);
+        }
     }
 
-    //Debug.DrawRay(OriginRaycast, direction * RaycastDistance, Color.red);
-
-    */
+    //Debug.DrawRay(OriginRaycast, Vector2.left * RaycastDistanceX, Color.red); //Pintar Raycast
+    //Debug.DrawRay(OriginRaycast, Vector2.up * RaycastDistanceY, Color.green); //Pintar Raycast
+    //Debug.DrawRay(OriginRaycast, Vector2.right * RaycastDistanceX, Color.yellow); //Pintar Raycast
+    //Debug.DrawRay(OriginRaycast, Vector2.down * RaycastDistanceY, Color.blue); //Pintar Raycast
 }
 
